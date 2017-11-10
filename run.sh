94114b412eb768919616a9e7ef77e0fc8d58ae8b
Leonel Castañeda <leodcastaneda@gmail.com>
2017-11-09 20:03:23 -0600

    set java_opts

--- run.sh
+++ run.sh
@@ -9,5 +9,11 @@ docker run --name postgis_boundless -e POSTGRES_PASSWORD=$DOCKER_POSTGIS_PASSWOR
 
 # boundless
 echo "building boundless in port $DOCKER_BOUNDLESS_PORT"
-docker build -t cedn/boundless .
-docker run --name boundless --link postgis_boundless:postgres -p $DOCKER_BOUNDLESS_PORT:8080 -d -t cedn/boundless
+docker build -t cedn/boundless . --build-arg geowebcache_conf=geowebcache.xml --build-arg geoserver_conf=geoserver.xml
+docker run --name boundless -e JAVA_OPTS='-Xms256m -Xmx756m -XX:SoftRefLRUPolicyMSPerMB=36000 -XX:-UsePerfData -Dorg.geotools.referencing.forceXY=true -Dorg.geotoools.render.lite.scale.unitCompensation=true' --link postgis_boundless:postgres -p $DOCKER_BOUNDLESS_PORT:8080 -d -t cedn/boundless
+
+
+
+
+
+# sudo docker run -d -p 8080:8080 -p 8009:8009 -v /opt/tomcat/webapps:/opt/tomcat/webapps dordoka/tomcat
